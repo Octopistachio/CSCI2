6c2c2e0d4324d64f9766203c66ad4e5af4a22972
Matt Wilson <mwilson2250@gmail.com>
2017-11-06 16:05:55 -0600

    Started NameSurferGUI

--- Assign5Names/src/NameSurferGUI.java
+++ Assign5Names/src/NameSurferGUI.java
@@ -1,2 +1,27 @@
-public class NameSurferGUI {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class NameSurferGUI extends Application {
+
+    private VBox root = new VBox(); //The root that holds each pane
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.setTitle("Names"); //Title of the window
+
+        primaryStage.setScene(new Scene(root,250,300));
+        primaryStage.setMinWidth(250); //Minimum width
+        primaryStage.setMinHeight(300); //Minimum height
+
+        primaryStage.show(); //Show the window
+
+    }
+
 }
