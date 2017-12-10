b37b3c31ad3ccb901292ef033ed1ee4da1f15d19
Octopistachio <mwilson2250@gmail.com>
2017-12-09 21:35:14 -0600

    Some Layout Changes and Worked on MorseCode

--- MorseCode/src/MorseCode.java
+++ MorseCode/src/MorseCode.java
@@ -1,64 +1,147 @@
-import javax.xml.soap.Node;
+import sun.reflect.generics.tree.Tree;
+
 import java.io.*;
-import java.util.Arrays;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class MorseCode {
 
+    public MorseCode() throws Exception {
+        TreeNode root = new TreeNode();
+        createMorseTree();
+    }
+
     private static TreeMap<Character, String> toCode = new TreeMap<>(); //Encodes characters to Morse Code
 
+    private static TreeNode root = new TreeNode();
+
+    public static class TreeNode {
+        private char letter;
+        private TreeNode left;
+        private TreeNode right;
+
+        public static final char NEWLETTER = '@';
+        public static final char SPACE = '*';
+        public static final char NEWLINE = '+';
+
+
+        public TreeNode() {
+            letter = NEWLETTER;
+            left = null;
+            right = null;
+        }
+
+        public char getLetter() { return letter; }
+        public void setLetter(char letter) { this.letter = letter; }
+
+        public TreeNode getLeft() { return left; }
+        public void setLeft(TreeNode left) { this.left = left; }
+
+        public TreeNode getRight() { return right; }
+        public void setRight(TreeNode right) { this.right = right; }
+    }
 
     public static void main(String[] args) throws Exception {
-        encodeFile("mcgee.txt", "mcgeeMyCopy.encoded");
+        encodeFile("mcgee.txt", "mcgeeMattCopy.encoded");
+        decodeFile("mcgee.encoded", "mcgeeMattCopy.txt");
     }
 
     public static void encodeFile(String inputFileName, String outputFileName) throws Exception {
 
+        PrintWriter writer = new PrintWriter(outputFileName); //The writer to the encoded file
+        File file = new File(inputFileName); //The file to be encoded's input
+        Scanner line = new Scanner(file); //The line reader
 
-        createMorseTree();
+        while (line.hasNextLine()) { //While there is a new line
 
-        PrintWriter writer = new PrintWriter(outputFileName);
-        File file = new File(inputFileName);
-        Scanner line = new Scanner(file);
+            char[] charArray = line.nextLine().toUpperCase().toCharArray(); //Make the line capital, and make it an array of characters
+            int tabSpacingMax = 0; //The max number of spaces before the sentence starts
+            int currentCount = 0; //The current count before the sentence begins
 
-        while (line.hasNextLine()) {
+            for(char character:charArray){ //For each character in the line
+                if(character != ' ') //If the character is not a space
+                    break; //Break the loop
+                tabSpacingMax++; //Else keep increasing the max
+            }
 
-            char[] charArray = line.toString().toUpperCase().toCharArray();
+            for (char character:charArray) { //For each character in the line
+                if(currentCount >= tabSpacingMax) { //If the current count is not equal to the max
+                    if (toCode.containsKey(character)) { //And if the current character is in the tree
+                        String encodedCharacter = toCode.get(character); //Pull the encoded character from the tree
+                        writer.write(encodedCharacter + "\n"); //And write it to the file
+                    }
+                }
 
-            for (char character:charArray) {
-                if(toCode.containsKey(character)) {
-                    String encodedCharacter = toCode.get(character);
-                    writer.write(encodedCharacter + "\n");
-                    System.out.println(character + " = " + toCode.get(character));
+                if(currentCount != tabSpacingMax) //If the current count does not equal the max
+                    currentCount++; //Increase the current count
+            }
+            writer.write('+' + "\n"); //When reaching the end of the line, add a +, indicating a new line
+        }
+        writer.close(); //Close the writer
+        line.close(); //Close the scanner
+    }
+
+    public static void decodeFile(String inputFileName, String outputFileName) throws Exception {
+
+        PrintWriter writer = new PrintWriter(outputFileName); //The writer to the encoded file
+        File file = new File(inputFileName); //The file to be encoded's input
+        Scanner line = new Scanner(file); //The line reader
+
+        String signal = "";
+        StringBuffer result = new StringBuffer();
+        TreeNode current = root;
+
+        while (line.hasNextLine()) { //While there is a new line
+            String value = line.nextLine();
+
+            for (int i = 0; i < value.length(); i++) {
+                signal = value.substring(i, i + 1);
+
+                if (signal.equals(".")) {
+                    System.out.println(".");
+                    if (current.getLeft() != null) {
+                        current = current.getLeft();
+                    } else {
+                        current.setLeft(new TreeNode());
+                        current = current.getLeft();
+                    }
+                } else if (signal.equals("-")) {
+                    System.out.println("-");
+                    if (current.getRight() != null) {
+                        current = current.getRight();
+                    } else {
+                        current.setRight(new TreeNode());
+                        current = current.getRight();
+                    }
+                } else {
+                    System.out.println("other");
+                    current = root;
                 }
             }
 
+            System.out.println(current.getLetter());
         }
-        writer.close();
-        line.close();
-
-
     }
 
-    private static void createMorseTree() throws Exception{
-        File file = new File("MorseCode.txt");
-        Scanner scanner = new Scanner(file);
+    private void createMorseTree() throws Exception {
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine().toUpperCase();
-            String[] lineArray = line.split("\\s+");
+        if (toCode.isEmpty()) { //If the tree map is empty
+            File file = new File("MorseCode.txt"); //Grab the MorseCode text file
+            Scanner scanner = new Scanner(file); //Create a scanner for the file
 
-            char character = lineArray[0].charAt(0);
-            String morseValue = lineArray[1];
+            while (scanner.hasNextLine()) { //While there is a new line
+                String line = scanner.nextLine().toUpperCase(); //Make the character uppercase
+                String[] lineArray = line.split("\\s+"); //And split the line by whitespaces
 
-            toCode.put(character, morseValue);
+                char character = lineArray[0].charAt(0); //Set the character to the first character on the line
+                String morseValue = lineArray[1]; //Set the morse value to the value from the file
+
+                toCode.put(character, morseValue); //Add the value and the character to the tree
+            }
+            scanner.close(); //Close the scanner
+
+            toCode.put(' ', "*"); //Manually add the space to the tree
         }
-        scanner.close();
-
-        toCode.put(' ', "*");
-        toCode.put('\n', "+");
     }
 }
 
